63c3e53f4a4cf5d23bf9bb64126e45992bce9e79
Liran Martfel <liran.martfel@gmail.com>
2026-02-25 12:47:44 +0200

    תיקון תרגיל

--- Homework_2.py
+++ Homework_2.py
@@ -1,15 +1,20 @@
 basic = []
 
+
 def basic_math (a = 10, b = 2):
     if b == 0:
         print (b, "is invalid")
         return 0
-    basic.append (a + b)
-    basic.append (a - b)
-    basic.append (a / b)
-    basic.append (a * b)
 
-basic_math ()
+    else:
+        results = []
+        results.append (a + b)
+        results.append (a - b)
+        results.append (a / b)
+        results.append (a * b)
+        return results
+
+basic = basic_math ()
 print (basic)
 
 # #option 2:
@@ -27,5 +32,5 @@ print (basic)
 #     basic.append (a / b)
 #     basic.append (a * b)
 
-basic_math (a,b)
-print (basic)
+#basic_math (a,b)
+#print (basic)
